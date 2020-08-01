2eef95f6096b9948a5bb939e0a66db4136d169bc
Azra <azra191996tabassum@gmail.com>
2020-08-01 22:06:21 +0530

    Updates

---
+++ precache-manifest.33a26b4cca72a2508d4e1b521b92f436.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81b3a830b1c9239f453bffce6e18095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7b9423d3fb9b97752b65",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9423d3fb9b97752b65",
+    "url": "/static/js/main.84933af3.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "e2b72c9eb9d71c196cabe4d50c0f30cb",
+    "url": "/static/media/resume.e2b72c9e.pdf"
+  }
+]);
